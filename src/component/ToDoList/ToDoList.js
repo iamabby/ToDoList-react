78809850d8f5bc6660d8663aa02294e2a2df336f
iamabby <1127153857@qq.com>
2018-12-04 08:47:23 +0800

    优优化

--- src/component/ToDoList/ToDoList.js
+++ src/component/ToDoList/ToDoList.js
@@ -1,6 +1,6 @@
 import React,{Component,Fragment} from "react";
 import "./ToDoList.scss";
-import ToDoItem from "../ToDoItem";
+import ToDoItem from "./ToDoItem";
 
 
 class ToDoList extends Component{
